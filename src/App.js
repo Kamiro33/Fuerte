93f951b95263691c9c9d924cf27b88dde7af2072
Kamiro33 <86894226+Kamiro33@users.noreply.github.com>
2024-11-18 15:52:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Register from './Pages/Register';
 import Dashboard from './Pages/Dashboard';
 import { WorkoutProvider } from './context/WorkoutContext'; // Contexto para datos compartidos
 import PlanWorkout from './components/Workouts/PlanWorkout'; // Componente PlanWorkout
+import Home from './Pages/Home'; // Componente para la ruta principal
 
 function App() {
   return (
     <WorkoutProvider>
       <Routes>
-        <Route path="/" element={<Login />} /> {/* Ruta principal hacia Login */}
+        <Route path="/" element={<Home />} /> {/* Ruta principal */}
+        <Route path="/login" element={<Login />} /> 
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/plan-workout" element={<PlanWorkout />} />
