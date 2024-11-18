4d9b5792d5078b06fc22b95c506b39f403d4fb87
Kamiro33 <86894226+Kamiro33@users.noreply.github.com>
2024-11-18 18:53:38 -0500

    Update Register.js

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-<<<<<<< HEAD
 import api from '../Services/api'; // Asume que tienes configurado Axios
 import styles from '../styles/register.module.css'; // Importar el archivo CSS como módulo
-=======
-import api from '../Services/api';
->>>>>>> c2e85787b88fe35d36970004ce8fc0fd83413caf
 
 function Register() {
   const [name, setName] = useState('');
@@ -30,7 +26,6 @@ function Register() {
   };
 
   return (
-<<<<<<< HEAD
     <div className={styles.registerContainer}>
       <div className={styles.registerForm}>
         <h2 className={styles.registerTitle}>Registro de Usuario</h2>
@@ -66,47 +61,12 @@ function Register() {
             />
           </div>
           {error && <p className={styles.errorMessage}>{error}</p>}
-          <button className={styles.registerButton} type="submit">Registrarse</button>
+          <button className={styles.registerButton} type="submit" disabled={loading}>
+            {loading ? 'Registrando...' : 'Registrarse'}
+          </button>
         </form>
         <p className={styles.registerLink}>¿Ya tienes cuenta? <a href="/">Inicia Sesión</a></p>
       </div>
-=======
-    <div>
-      <h2>Registro de Usuario</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nombre:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Contraseña:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="submit" disabled={loading}>
-          {loading ? 'Registrando...' : 'Registrarse'}
-        </button>
-      </form>
->>>>>>> c2e85787b88fe35d36970004ce8fc0fd83413caf
     </div>
   );
 }
